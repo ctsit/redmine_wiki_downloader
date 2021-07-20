b99dcb91db032a7e0e027a4ec87cfaee40678b84
Kyle Chesney <kyle.2493@gmail.com>
2021-07-20 16:56:56 -0400

    Ensure proper linking of nested wiki pages when linked in the content of a higher level page

--- convert_wiki_to_md.py
+++ convert_wiki_to_md.py
@@ -2,6 +2,7 @@
 
 import re
 import argparse
+import os
 
 def replace_redmine_wiki_with_textile_link(page_content):
     """
@@ -14,11 +15,23 @@ def replace_redmine_wiki_with_textile_link(page_content):
                             # link text
                             '"' + match.expand(r"\1") + '"' +
                             # link path
-                            # ':' + match.expand(r"\1").replace(" ", "_") + ".md"
-                            ':' + match.expand(r"\1").replace(" ", "_") + "/"
+                            ':' + find_nested_wiki_path(match.expand(r"\1").replace(" ", "_")) + "/"
                             ),
                     string = page_content)
     return proper
+
+
+def find_nested_wiki_path(search):
+    search_file = search + ".textile"
+    for root, dirs, files in os.walk(os.path.dirname(args.input_file)):
+        if (search_file in files):
+            wiki_path = os.path.join(root, search)
+            # remove everything leading up to the project root to allow a proper path
+            nested_path = wiki_path.replace(os.path.dirname(args.input_file), "")[1:]
+            return(nested_path)
+
+    return search
+
 
 def update_file(filename):
     with open(filename) as f:
@@ -26,6 +39,7 @@ def update_file(filename):
     proper = replace_redmine_wiki_with_textile_link(content)
     with open(filename, 'w') as f:
         f.write(proper)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
